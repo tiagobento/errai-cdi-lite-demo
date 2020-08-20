064aa8b945365fd890ca06e6464c086a45d72e24
Tiago Bento <tiagobentofernandes@gmail.com>
2020-08-20 13:24:15 -0300

    Adding j2cl start

---
+++ todolist-j2cl/src/main/java/org/treblereel/App.java
@@ -0,0 +1,16 @@
+package org.treblereel;
+
+import elemental2.dom.DomGlobal;
+import elemental2.dom.Event;
+import elemental2.dom.EventListener;
+import elemental2.dom.HTMLButtonElement;
+
+public class App {
+
+    public void onModuleLoad() {
+        HTMLButtonElement btn = (HTMLButtonElement) DomGlobal.document.createElement("button");
+        btn.textContent = "CLICK ME22222";
+        btn.addEventListener("click", evt -> DomGlobal.window.alert("HELLO WORLD!"));
+        DomGlobal.document.body.appendChild(btn);
+    }
+}
